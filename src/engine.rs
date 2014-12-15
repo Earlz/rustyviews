e0fddf15075e11788e20c9c8b66bf84f365c8a8a
Earlz <earlz@earlz.net>
2014-12-15 02:20:59 -0500

    output stuff

--- src/engine.rs
+++ src/engine.rs
@@ -5,11 +5,14 @@ pub struct ViewEngine {
   pub template: String
 }
 impl ViewEngine {
-    pub fn new(template: String) -> ViewEngine {
+  pub fn new(template: String) -> ViewEngine {
     let e=ViewEngine{ template: template};
     e
   }
-  pub fn generate(&self, output: &Writer) -> String {
+  pub fn generate(&self, output: &mut Writer) -> String {
+    for c in self.template.chars() {
+      output.write_char(c);
+    }
     "".to_string()
   }
 }
